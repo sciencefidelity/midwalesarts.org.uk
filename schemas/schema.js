fc97b98534b54d1f51cbf0a453ae43b462cd11a3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-18 02:02:18 +0000

    Move taxonomy schema into their own directory

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,10 +1,7 @@
-// First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
-
-// Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// We import object and document schemas
+// localization
 import { translateFields } from './fieldTranslation'
 // objects
 import blockContent from './objects/blockContent'
@@ -13,39 +10,38 @@ import localeSlug from './objects/localeSlug'
 // documents
 import artist from './documents/artist'
 import artwork from './documents/artwork'
-import category from './documents/category'
-import discipline from './documents/discipline'
 import exhibition from './documents/exhibition'
 import frontPage from './documents/frontPage'
 import frontPageSection from './documents/frontPageSection'
 import post from './documents/post'
 import space from './documents/space'
 import video from './documents/video'
+// taxonomy
+import category from './taxonomy/category'
+import discipline from './taxonomy/discipline'
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes
+  
     .concat(translateFields([
-      // The following are document types which will appear
-      // in the studio.
+      // documents
       artist,
       artwork,
-      category,
-      discipline,
       exhibition,
       frontPage,
       frontPageSection,
       space,
       post,
       video,
+      
+      // taxonomy
+      category,
+      discipline,
     ]))
-      // When added to this list, object types can be used as
-      // { type: 'typename' } in other document schemas
+
     .concat([
+      // objects
       blockContent,
       captionImage,
       localeSlug,
