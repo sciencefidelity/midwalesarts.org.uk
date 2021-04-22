99cb329423e3241c00a35cf55ff7d27a837c4da2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-22 23:46:39 +0000

    Create span for each letter

--- web/src/components/brand.tsx
+++ web/src/components/brand.tsx
@@ -7,8 +7,19 @@ const Brand = () => (
       <span>i</span>
       <span>d</span>
     </h1>
-    <h1 className="brand">Wales</h1>
-    <h1 className="brand bold">Arts</h1>
+    <h1 className="brand">
+      <span>W</span>
+      <span>a</span>
+      <span>l</span>
+      <span>e</span>
+      <span>s</span>
+    </h1>
+    <h1 className="brand bold">
+      <span>A</span>
+      <span>r</span>
+      <span>t</span>
+      <span>s</span>
+    </h1>
   </>
 )
 
