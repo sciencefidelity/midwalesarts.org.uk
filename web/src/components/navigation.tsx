cc4ed5a84227321acdd376632ad9c0f15966b1b2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-23 20:05:57 +0000

    Add placeholder for language switcher

--- web/src/components/navigation.tsx
+++ web/src/components/navigation.tsx
@@ -3,6 +3,7 @@ import React from "react"
 const Navigation = () => (
   <div className="hamburgerContainer">
     <div className="hamburger"></div>
+    <div className="languageSwitcher">CY</div>
   </div>
 )
 
