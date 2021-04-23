61e432ffe4ade48a8eab629114edd41d37401a72
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-23 23:40:26 +0000

    Add social links

--- web/src/components/navigation.tsx
+++ web/src/components/navigation.tsx
@@ -1,9 +1,36 @@
 import React from "react"
 
 const Navigation = () => (
-  <div className="hamburgerContainer">
+  <div>
     <div className="hamburger"></div>
     <div className="languageSwitcher">CY</div>
+    <div className="social">
+      <a href="#">
+        <img
+          alt=""
+          src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'%3E%3Cpath d='M400 32H48A48 48 0 000 80v352a48 48 0 0048 48h137V328h-63v-72h63v-55c0-62 37-96 94-96 27 0 55 5 55 5v61h-31c-31 0-40 19-40 38v47h69l-11 72h-58v152h137a48 48 0 0048-48V80a48 48 0 00-48-48z' fill='%23094754'/%3E%3C/svg%3E"
+          loading="lazy"
+        />
+      </a>
+    </div>
+    <div className="social">
+      <a href="#">
+        <img
+          alt=""
+          src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'%3E%3Cpath d='M224 141a115 115 0 100 229 115 115 0 000-229zm0 190a75 75 0 110-150 75 75 0 010 150zm147-195a27 27 0 11-54 0 27 27 0 0154 0zm76 28c-2-36-10-68-37-94-26-27-58-35-93-37-37-2-148-2-185 0-36 2-68 10-94 36S3 129 1 164c-2 37-2 148 0 185 2 36 10 68 37 94s58 35 94 36c37 3 148 3 185 0 35-1 67-9 93-36 27-26 35-58 37-94 2-37 2-147 0-184zm-48 224c-8 20-23 35-43 43-29 11-99 9-132 9s-103 2-132-9c-20-8-35-23-43-43-11-29-9-99-9-132s-2-103 9-132c8-20 23-35 43-43 30-11 100-9 132-9s103-2 132 9c20 8 35 23 43 43 12 29 9 99 9 132s3 103-9 132z' fill='%23094754'/%3E%3C/svg%3E"
+          loading="lazy"
+        />
+      </a>
+    </div>
+    <div className="social">
+      <a href="#">
+        <img
+          alt=""
+          src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'%3E%3Cpath d='M400 32H48C22 32 0 54 0 80v352c0 27 22 48 48 48h352c27 0 48-21 48-48V80c0-26-21-48-48-48zm-49 159v8c0 87-66 187-186 187-37 0-72-11-101-30a138 138 0 0097-27c-29 0-53-19-61-45 10 1 19 1 30-1-30-6-53-33-53-65v-1c9 5 19 8 30 9a65 65 0 01-30-55c0-12 4-23 9-33 33 40 81 66 136 69a66 66 0 01111-60c15-3 29-9 42-16-5 15-15 28-29 36 13-1 26-5 38-10-9 13-20 25-33 34z' fill='%23094754'/%3E%3C/svg%3E"
+          loading="lazy"
+        />
+      </a>
+    </div>
   </div>
 )
 
