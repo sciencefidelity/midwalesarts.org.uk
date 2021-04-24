b17b5752ae2c24ed1628b5d1517dcdb04b71441b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-24 01:51:29 +0000

    Animate the hamburger

--- web/src/components/headerMenu.tsx
+++ web/src/components/headerMenu.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const HeaderMenu = () => (
-  <div className="headerMenu">
+  <div className="headerMenu hidden">
     <div className="marker"></div>
     <ul>
       <li><a href="#">Mid Wales Arts</a></li>
