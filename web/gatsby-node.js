c76b553f102425e91223c8eaa7e8bb949ed71849
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-27 05:48:48 +0000

    Create empty pages

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -4,7 +4,35 @@ exports.createPages = ({ actions: { createPage }}) => {
     component: require.resolve('./src/pages/artists.tsx')
   })
   createPage({
+    path: '/about/',
+    component: require.resolve('./src/pages/about.tsx')
+  })
+  createPage({
+    path: '/events/',
+    component: require.resolve('./src/pages/events.tsx')
+  })
+  createPage({
     path: '/exhibitions/',
     component: require.resolve('./src/pages/exhibitions.tsx')
+  })
+  createPage({
+    path: '/news/',
+    component: require.resolve('./src/pages/news.tsx')
+  })
+  createPage({
+    path: '/support/',
+    component: require.resolve('./src/pages/support.tsx')
+  })
+  createPage({
+    path: '/videos/',
+    component: require.resolve('./src/pages/videos.tsx')
+  })
+  createPage({
+    path: '/visit/',
+    component: require.resolve('./src/pages/visit.tsx')
+  })
+  createPage({
+    path: '/workshops/',
+    component: require.resolve('./src/pages/workshops.tsx')
   })
 }
