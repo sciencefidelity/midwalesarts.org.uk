a36cbf6a99b40cf3e924251fc4671f0d1fa1afe7
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-28 02:50:26 +0000

    Filter data in the posts page

--- web/src/templates/post.tsx
+++ web/src/templates/post.tsx
@@ -1,21 +1,43 @@
 import * as React from "react"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-const PostPage = ({ pageContext }) => (
-  <>
+export const query = graphql `
+  query SinglePostQuery($slug: String!) {
+    sanityPost(slug: {en: {current: { eq: $slug}}}) {
+      body {
+        _rawEn(resolveReferences: {maxDepth: 10})
+      }
+      id
+      image {
+        asset {
+          gatsbyImageData(width: 1440, formats: WEBP, placeholder: BLURRED)
+        }
+      }
+      publishedAt
+      title {
+        en
+      }
+    }
+  }
+`
+
+const PostPage = props => {
+  const { data } = props
+  const post = data && data.sanityPost
+  return (
     <Layout
-      heroImage={pageContext.mainImage}
+      heroImage={post.image.asset.gatsbyImageData}
       heroImageCaption="&nbsp;"
-      heroTitle={pageContext.date}
-      heroCaption="&nbsp;"
+      heroTitle="News"
+      heroCaption={post.title.en}
     >
       <section>
         
       </section>
     </Layout>
-  </>
-)
+  )
+}
 
 export default PostPage
