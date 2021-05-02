dac8d53b963402511b045f70ff504d5bc14e2225
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-02 16:22:11 +0000

    Work on tabs for artists page

--- web/src/templates/artist.tsx
+++ web/src/templates/artist.tsx
@@ -1,13 +1,18 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { graphql, Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/layout'
 import PortableText from '../components/portableText'
 
+import '../scss/artist.scss'
+
 export const query = graphql `
   query singleArtistQuery($name: String!) {
-    allSanityArtwork(filter: {artist: { eq: $name } }) {
+    allSanityArtwork(
+      sort: {fields: date, order: DESC}
+      filter: {artist: { eq: $name }}
+    ) {
       edges {
         node {
           id
@@ -50,8 +55,16 @@ export const query = graphql `
 `
 
 const ArtistPage = ({ data }) => {
-  const artist = data && data.sanityArtist
-  const artwork = data && data.allSanityArtwork
+  const [isBio, setBio] = useState(true)
+  const [isImage, setImage] = useState(false)
+
+  const toggleTabs = () => {
+    setBio(!isBio)
+    setImage(!isImage)
+  }
+  
+  const artist = data.sanityArtist
+  const artwork = data.allSanityArtwork
   return (
     <Layout
       heroImage={artist.mainImage.asset.gatsbyImageData}
@@ -59,23 +72,27 @@ const ArtistPage = ({ data }) => {
     >
       <section>
         <div className="sidebarContainer">
-          <div className="portableContainer artistBio">
+          <div className="portableContainer">
             <h1>Artist</h1>
             <p className="subTitle">{artist.title}</p>
-            {artist.body._rawEn && <PortableText blocks={artist.body._rawEn} />}
+            <ul className="artistMenu">
+              <li onClick={toggleTabs}>Works</li>
+              <li onClick={toggleTabs}>Biography</li>
+            </ul>
+            <div className={isBio ? "hidden artistBio" : "artistBio"} >
+              {artist.body._rawEn && <PortableText blocks={artist.body._rawEn} />}
+            </div>
           </div>
         </div>
-        <div className="imageGrid">
+        <div className={isImage ? "hidden artistImageGrid" : "artistImageGrid"}>
           {artwork.edges.map(artworks => (
-            <>
-              <div style={{margin: 0}}>
-                <GatsbyImage 
-                  image={artworks.node.mainImage.asset.gatsbyImageData}
-                  alt=""
-                />
-                <div className="gridCaption">{artworks.node.title.en} ({artworks.node.date})</div>
-              </div>
-            </>
+            <div style={{margin: 0}}>
+              <GatsbyImage 
+                image={artworks.node.mainImage.asset.gatsbyImageData}
+                alt=""
+              />
+              <div className="gridCaption">{artworks.node.title.en} ({artworks.node.date})</div>
+            </div>
           ))}
         </div>
         <div><p className="backLink"><Link to="/artists/">Back to Artists</Link></p></div>
