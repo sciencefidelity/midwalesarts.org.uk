7b18ca60b2f8aaecab4bceff5751bc3c54983f40
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-16 18:20:07 +0000

    Add descriptions for editors

--- studio/schemas/documents/artist.ts
+++ studio/schemas/documents/artist.ts
@@ -6,22 +6,26 @@ export default {
     {
       name: 'title',
       title: 'Name',
+      description: 'Artist full name.',
       type: 'string',
     },
     {
       name: 'body',
       title: 'Biography',
+      description: 'Artist biography or artistic statment, choose Quote from the dropdown in the top left for statements.',
       type: 'localeRichText',
     },
     {
       name: 'disciplines',
       title: 'Disciplines',
+      description: 'Choose from the dropdown. Add disciplines in the Discipline section.',
       type: 'array',
       of: [{type: 'reference', to: {type: 'discipline'}}],
     },
     {
       name: 'slug',
       title: 'Slug',
+      description: 'Click Generate.',
       type: 'slug',
       options: {
         source: 'title',
@@ -31,13 +35,14 @@ export default {
     {
       name: 'mainImage',
       title: 'Main image',
+      description: 'Images should be jpeg of 1440px along the longest edge, 500-600k is best.',
       type: 'captionImage',
       options: {
         hotspot: true,
       },
     },
   ],
-  
+
   preview: {
     select: {
       title: 'title',
