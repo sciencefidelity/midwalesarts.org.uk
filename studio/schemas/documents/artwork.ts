1c9e4ed5629cbd3b8be804d9ccca2cf8e9bbf812
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-16 18:28:27 +0000

    Add descriptions for content editors

--- studio/schemas/documents/artwork.ts
+++ studio/schemas/documents/artwork.ts
@@ -26,27 +26,32 @@ export default {
     {
       name: 'date',
       title: 'Date',
+      description: 'The year the work was created.',
       type: 'string',
     },
     {
       name: 'price',
       title: 'Price',
+      description: 'Add £ before the price, use POA for enquire only, and NFS for not for sale.',
       type: 'string',
     },
     {
       name: 'sold',
       title: 'Sold?',
+      description: 'Sold artworks will not display on the artist pages.',
       type: 'boolean',
     },
     {
       name: 'exhibition',
       title: 'In exhibition',
+      description: 'Before adding artworks to a new exhibition, add the exhibition in the Exhibitions section.',
       type: 'array',
       of: [{type: 'reference', to: {type: 'exhibition'}}],
     },
     {
       name: 'mainImage',
       title: 'Main image',
+      description: 'Images should be jpeg of 1440px along the longest edge, 500-600k is best.',
       type: 'image',
       options: {
         hotspot: true,
