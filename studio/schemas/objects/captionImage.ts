a5cd5e29eddd6331f3e766176b767755b899c3bb
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-20 22:24:43 +0000

    Add descriptions in studio

--- studio/schemas/objects/captionImage.ts
+++ studio/schemas/objects/captionImage.ts
@@ -2,6 +2,7 @@ export default {
   title: 'Image',
   name: 'captionImage',
   type: 'image',
+  description: 'Images should be jpeg of 1440px along the longest edge, 500-600k is best.',
   fields: [
     {
       name: 'caption',
