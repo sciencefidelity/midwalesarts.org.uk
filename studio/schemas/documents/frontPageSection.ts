420f5901ce105fcfd7fff42d5defeac156d0afdc
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-20 22:18:52 +0000

    Add character limits and discriptions

--- studio/schemas/documents/frontPageSection.ts
+++ studio/schemas/documents/frontPageSection.ts
@@ -10,33 +10,43 @@ export default {
     {
       name: 'order',
       title: 'Order',
-      description: 'The order that items appear on the fromt page (1 to 4)',
+      description: 'The order that items appear on the front page (1 is first)',
       type: 'number',
     },
     {
       name: 'title',
       title: 'Headline',
       type: 'localeString',
+      description: 'Limit 15 characters.',
+      validation: (Rule:any) => Rule.max(15).warning(`Limit 35 characters.`),
     },
     {
       name: 'caption',
       title: 'Subheading',
       type: 'localeString',
+      description: 'Limit 35 characters.',
+      validation: (Rule:any) => Rule.max(35).warning(`Limit 35 characters.`),
     },
     {
       name: 'heading',
       title: 'Heading',
+      description: 'Limit 30 characters.',
       type: 'localeString',
+      validation: (Rule:any) => Rule.max(30).warning(`Limit 30 characters.`),
     },
     {
       name: 'body',
       title: 'Body',
+      description: 'Limit 400 characters.',
       type: 'localeRichText',
+      validation: (Rule:any) => Rule.max(400).warning(`Limit 400 characters.`),
     },
     {
       name: 'cta',
       title: 'Call to Action',
+      description: 'A call to action for the internal link (20 characters max).',
       type: 'localeString',
+      validation: (Rule:any) => Rule.max(20).warning(`Maximum 20 characters.`),
     },
     {
       name: 'ctaLink',
@@ -46,6 +56,7 @@ export default {
     {
       name: 'mainImage',
       title: 'Main image',
+      description: 'Images should be jpeg of 1440px along the longest edge, 500-600k is best.',
       type: 'captionImage',
       options: {
         hotspot: true,
@@ -54,6 +65,7 @@ export default {
     {
       name: 'subImage',
       title: 'Inset image',
+      description: 'Images should be jpeg of 1440px along the longest edge, 500-600k is best.',
       type: 'captionImage',
       options: {
         hotspot: true,
