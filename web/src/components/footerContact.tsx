68d272dda33e0d34abcd22ebdeb31709b0e4df54
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-23 23:56:19 +0000

    Reformat/prettify

--- web/src/components/footerContact.tsx
+++ web/src/components/footerContact.tsx
@@ -5,10 +5,22 @@ const year = new Date().getFullYear()
 const FooterContact = () => {
   return (
     <div>
-      <p>Opening Times<br/>Thursday - Sunday 11-4</p>
-      <p>Mid Wales Arts<br/>Caersws<br/>Powys SY17 5SB</p>
-      <p>+44 (0) 1686 688369<br/><a href="mailt:office@midwalesarts">office@midwalesarts.org.uk</a></p>
-      <p className="copy">&copy; Mid Wales Arts {year}</p>
+      <p>
+        Opening Times
+        <br/>Thursday - Sunday 11-4
+      </p>
+      <p>
+        Mid Wales Arts
+        <br/>Caersws
+        <br/>Powys SY17 5SB
+      </p>
+      <p>
+        +44 (0) 1686 688369
+        <br/><a href="mailt:office@midwalesarts">office@midwalesarts.org.uk</a>
+      </p>
+      <p className="copy">
+        &copy; Mid Wales Arts {year}
+      </p>
     </div>
   )
 }
