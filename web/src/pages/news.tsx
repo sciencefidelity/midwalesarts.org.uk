e37fb1d4f1109aa2ab44576d6173cdd749bfef48
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-07-27 18:05:10 +0000

    Remove unused variable

--- web/src/pages/news.tsx
+++ web/src/pages/news.tsx
@@ -22,7 +22,7 @@ const News = () => (
           <div className="imageGrid">
             {!!data.newsMain.edges &&
               data.newsMain.edges.map(
-                (posts: any, index: number) =>
+                (posts: any) =>
                   !!posts && (
                     <div key={posts.node.id} style={{ margin: 0 }}>
                       <Link to={`/news/${posts.node.slug.en.current}/`}>
