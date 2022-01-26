2a2e3bcf8930f07c32f2eb03eb67348e8f7fad4a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-26 08:02:38 +0000

    chore: remove unused files and packages

--- dev/lib/config.ts
+++
@@ -1,6 +0,0 @@
-export const sanityConfig = {
-  projectId: process.env.SANITY_PROJECT_ID || "",
-  dataset: process.env.SANITY_DATASET || "production",
-  apiVersion: "2021-09-20",
-  useCdn: true
-}
