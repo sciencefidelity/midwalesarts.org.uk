02b00a9ee7ce4a6aedbe27b63a0f106b9d4524ef
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-29 23:01:11 +0000

    fix: change api key to env and slightly fix styling of modal images

--- web/components/sidebar.tsx
+++ web/components/sidebar.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 import type { Event, Exhibition, Post } from "generated/schema"
+import Localize from "components/localize"
 
 const Sidebar = ({
   events,
@@ -9,86 +11,85 @@ const Sidebar = ({
   events: Event[]
   exhibitions: Exhibition[]
   posts: Post[]
-}) => (
-  <aside className="sidebar">
-    <div className="sidebarContent">
-      <h3>Latest News</h3>
-      <ul className="sidebarMenu">
-        {posts &&
-          posts.map(
-            post =>
-              post && (
-                <div key={post._id}>
+}) => {
+  const { locale } = useRouter()
+  return (
+    <aside className="sidebar">
+      <div className="sidebarContent">
+        <h3>{locale === "cy" ? "Y newyddion diweddaraf" : "Latest News"}</h3>
+        <ul className="sidebarMenu">
+          {posts &&
+            posts.map(
+              post =>
+                post && (
                   <Link href={`/news/${post.slug.en.current}/`}>
-                    <a>
-                      <li>{post.title.en}</li>
+                    <a href="default" key={post._id}>
+                      <li>
+                        <Localize data={post.title} />
+                      </li>
                     </a>
                   </Link>
-                </div>
-              )
-          )}
-      </ul>
-      <h3>Upcoming Events</h3>
-      <ul className="sidebarMenu">
-        {events &&
-          events.map(
-            event =>
-              event && (
-                <div key={event._id}>
+                )
+            )}
+        </ul>
+        <h3>{locale === "cy" ? "Digwyddiadau i ddod" : "Upcoming Events"}</h3>
+        <ul className="sidebarMenu">
+          {events &&
+            events.map(
+              event =>
+                event && (
                   <Link href={`/events/${event.slug.en.current}/`}>
-                    <a>
-                      <li>{event.title.en}</li>
+                    <a href="default" key={event._id}>
+                      <li><Localize data={event.title} /></li>
                     </a>
                   </Link>
-                </div>
-              )
-          )}
-      </ul>
-      <h3>Exhibitions</h3>
-      <ul className="sidebarMenu">
-        {exhibitions &&
-          exhibitions.map(
-            exhibition =>
-              exhibition && (
-                <div key={exhibition._id}>
+                )
+            )}
+        </ul>
+        <h3>{locale === "cy" ? "Arddangosfeydd" : "Exhibitions"}</h3>
+        <ul className="sidebarMenu">
+          {exhibitions &&
+            exhibitions.map(
+              exhibition =>
+                exhibition && (
                   <Link href={`/exhibitions/${exhibition.slug.en.current}/`}>
-                    <a>
-                      <li>{exhibition.title.en}</li>
+                    <a href="default" key={exhibition._id}>
+                      <li><Localize data={exhibition.title} /></li>
                     </a>
                   </Link>
-                </div>
-              )
-          )}
-      </ul>
-      <h3>Weekly Clubs</h3>
-      <ul className="sidebarMenu">
-        <Link href="/workshop">
-          <a>
-            <li>Wednesdays: Home Ed Group</li>
-          </a>
-        </Link>
-        <Link href="/workshops">
-          <a>
-            <li>Wednesdays: Ceramics Club</li>
-          </a>
-        </Link>
-        <Link href="/workshops">
-          <a>
-            <li>Thursdays: Adult Pottery</li>
-          </a>
-        </Link>
-        <Link href="/workshops">
-          <a>
-            <li>Thursdays: After School Club</li>
-          </a>
-        </Link>
-        <Link href="/workshops">
-          <a>
-            <li>Saturdays: Family Workshop</li>
-          </a>
-        </Link>
-      </ul>
-    </div>
-  </aside>
-)
+                )
+            )}
+        </ul>
+        <h3>{locale === "cy" ? "Clybiau Wythnosol" : "Weekly Clubs"}</h3>
+        <ul className="sidebarMenu">
+          <Link href="/workshop">
+            <a href="default">
+              <li>{locale === "cy" ? "Dydd Mercher: Home Ed Group" : "Wednesdays: Home Ed Group"}</li>
+            </a>
+          </Link>
+          <Link href="/workshops">
+            <a href="default">
+              <li>{locale === "cy" ? "Dydd Mercher: Clwb Serameg" : "Wednesdays: Ceramics Club"}</li>
+            </a>
+          </Link>
+          <Link href="/workshops">
+            <a href="default">
+              <li>{locale === "cy" ? "Dydd Iau: Crochenwaith Oedolion" : "Thursdays: Adult Pottery"}</li>
+            </a>
+          </Link>
+          <Link href="/workshops">
+            <a href="default">
+              <li>{locale === "cy" ? "Dydd Iau: Clwb ar ôl Ysgol" : "Thursdays: After School Club"}</li>
+            </a>
+          </Link>
+          <Link href="/workshops">
+            <a href="default">
+              <li>{locale === "cy" ? "Dydd Sadwrn: Gweithdy Teulu" : "Saturdays: Family Workshop"}</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+    </aside>
+  )
+}
 export default Sidebar
