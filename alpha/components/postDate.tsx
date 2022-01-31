1b0dcfbd1388fa72bb9c05adf3e9885ed175c32a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-31 17:20:10 +0000

    refactor: create component for formatting dates with locale

---
+++ alpha/components/postDate.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router"
+import format from "date-fns/format"
+import { cy, enGB } from "date-fns/locale"
+
+const PostDate = ({ date }) => {
+  const { locale } = useRouter()
+  return (
+    <>
+      {locale === "cy"
+        ? format(new Date(date),
+          "eeee, MMMM Do yyyy",
+          {locale: cy}
+        )
+        : format(new Date(date),
+          "eeee, MMMM Do yyyy",
+          {locale: enGB}
+        )
+      }
+    </>
+  )
+}
+export default PostDate
