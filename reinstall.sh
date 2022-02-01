a911bef356c57ff11000305e129344709452c975
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-01 12:14:53 +0000

    ci: update and run install script

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # delete all node modules and reinstall after dependabot
-rm -rf ./node_modules &&
+rm -rf ./node_modules yarn.lock &&
 yarn &&
-rm -rf */node_modules &&
+rm -rf */node_modules yarn.lock &&
 for d in */; do cd "$d"; yarn; cd ..; done
