839a4375ab541f6a8e3bb45e0d15c0002ec27ce2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-18 15:47:22 +0000

    chore: remove archived project

--- beta/src/components/portableText.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import clientConfig from "../../client-config"
-import BasePortableText from "@sanity/block-content-to-react"
-
-const PortableText = ({ blocks }) => (
-  <BasePortableText blocks={blocks} {...clientConfig.sanity} />
-)
-
-export default PortableText
