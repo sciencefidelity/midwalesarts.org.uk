b7eb8e6061f88a395f9cc2c5580e8c4e04ea68f5
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-19 22:38:50 +0100

    refactor: list artists alphabetically

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -1,12 +1,13 @@
 import imageUrlBuilder from "@sanity/image-url"
-import sanityClient from "@/lib/sanityClient"
+import sanityClient from "lib/sanityClient"
 import { LocaleRichText, LocaleString } from "generated/schema"
-import { Image } from "@/lib/interfaces"
+import { Image, SlugProps } from "lib/interfaces"
+import { Artist } from "generated/schema"
 
-export const buildUrl = (slugData: any) => {
-  const slug = slugData.slug && slugData.slug.en
-    ? slugData.slug.en.current
-    : slugData.slug.current
+export const buildUrl = (slugData: SlugProps): string => {
+  let slug = ""
+  if (slugData?.slug?.current) slug = slugData?.slug?.current
+  if (slugData?.slug?.en?.current) slug = slugData?.slug?.en?.current
   let directory: string
   switch (slugData._type) {
   case "artist":
@@ -30,12 +31,11 @@ export const buildUrl = (slugData: any) => {
   return `${directory}/${slug}`
 }
 
-export const capitalize = (word: string): string => {
-  return word[0].toUpperCase() + word.slice(1, word.length)
-}
-
-export const kebabCase = (word: string) => {
-  return word.toLowerCase().split(" ").join("-").replace(/[^a-z0-9-]/g, "")
+export const capitalize = (str: string): string => {
+  return str
+    .split(/[\s-]/g)
+    .map(e => e.replace(e[0], e[0].toUpperCase()))
+    .join(" ")
 }
 
 export const localize = (
@@ -47,44 +47,15 @@ export const localize = (
     : content.en
 }
 
+export const sortArtists = (artists: Artist[]): Artist[] => {
+  return artists.sort((a, b) => {
+    return a.title
+      .trim()
+      .replace(/(^\b\w+\s)/gi, "")
+      .localeCompare(b.title.trim().replace(/(^\b\w+\s)/gi, ""))
+  })
+}
+
 export const urlFor = (source: Image) => {
   return imageUrlBuilder(sanityClient).image(source)
-}
-
-// https://stackoverflow.com/questions/5845238/javascript-generate-transparent-1x1-pixel-in-dataurl-format/33919020#33919020
-
-export const hexDataURL = (hexColor: string) => {
-  return `data:image/gif;base64,R0lGODlhAQABAPAA${encodeHex(
-    hexColor
-  )}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
-}
-
-const keyStr =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-
-const encodeHex = (hexColor: string) => {
-  const s = hexColor.substring(1, 7)
-  const rgb = [
-    parseInt(s[0] + s[1], 16),
-    parseInt(s[2] + s[3], 16),
-    parseInt(s[4] + s[5], 16)
-  ]
-  return encodeRGB(rgb[0], rgb[1], rgb[2])
-}
-
-const encodeRGB = (r: number, g: number, b: number) => {
-  return triplet(0, r, g) + triplet(b, 255, 255)
-}
-
-const triplet = (e1: number, e2: number, e3: number) => {
-  const enc1 = e1 >> 2
-  const enc2 = ((e1 & 3) << 4) | (e2 >> 4)
-  const enc3 = ((e2 & 15) << 2) | (e3 >> 6)
-  const enc4 = e3 & 63
-  return (
-    keyStr.charAt(enc1) +
-    keyStr.charAt(enc2) +
-    keyStr.charAt(enc3) +
-    keyStr.charAt(enc4)
-  )
 }
