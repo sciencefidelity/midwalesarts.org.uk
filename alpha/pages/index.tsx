7d1b407d537fd269da5739d7c1ca9f1a91150ffe
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-25 22:07:17 +0100

    refactor: run prettier

--- alpha/pages/index.tsx
+++ alpha/pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      Hello World!
-    </div>
-  )
+  return <div>Hello World!</div>
 }
 
 export default Home
