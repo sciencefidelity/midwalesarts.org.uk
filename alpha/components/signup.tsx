b7b88592ffd1225ab993b91255f5d1d2a9fa1be6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-27 22:59:59 +0100

    refactor: convert footer styles to modules

--- alpha/components/signup.tsx
+++ alpha/components/signup.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react"
 import { useRouter } from "next/router"
 import { Label } from "lib/interfaces"
+import s from "styles/layout.module.scss"
 
 interface Props {
   labels: Label[]
@@ -10,17 +11,16 @@ interface Props {
 export const Signup: FC<Props> = ({ labels }) => {
   const { locale } = useRouter()
   return (
-    <div className="signup">
-      <div className="signupHeading">
+    <div className={`${s.signup}`}>
+      <div className={`${s.signupHeading}`}>
         {labels[5].text[locale]}
       </div>
-      <div className="signupForm">
+      <div className={`${s.signupForm}`}>
         <form
           action="https://midwalesarts.us8.list-manage.com/subscribe/post?u=886d00d2217d67cd8145db87b&amp;id=625e7cefb6"
           method="post"
           id="mc-embedded-subscribe-form"
           name="mc-embedded-subscribe-form"
-          className="validate"
           target="_blank"
           noValidate
         >
@@ -28,12 +28,12 @@ export const Signup: FC<Props> = ({ labels }) => {
             type="text"
             name="email"
             placeholder={labels[6].text[locale]}
-            className="signupInput"
+            className={`${s.signupInput}`}
           />
           <input
             type="image"
             src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'%3E%3Cpath d='M285 273L91 467c-9 10-24 10-34 0l-22-22c-10-10-10-25-1-34l155-155L34 101c-9-9-9-24 1-34l22-22c10-10 25-10 34 0l194 194c10 9 10 25 0 34z' fill='%23fff'/%3E%3C/svg%3E"
-            className="signupSubmit"
+            className={`${s.signupSubmit}`}
             name="subscribe"
             id="mc-embedded-subscribe"
             alt={labels[7].text[locale]}
