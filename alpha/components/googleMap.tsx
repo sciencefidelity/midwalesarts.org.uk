03ee55e6f2aa4009ce58f1a92fe87e36bd5cf830
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-28 16:39:53 +0100

    refactor: convert all styles on visit page to modules

---
+++ alpha/components/googleMap.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react"
+import GoogleMapReact from "google-map-react"
+import { LocationPin } from "components/locationPin"
+
+export const GoogleMap: FC = () => {
+  const location = {
+    address: "Mid Wales Arts Centre, Maesmawr, Caesws, Newtown SY17 5SB",
+    lat: 52.512,
+    lng: -3.425
+  }
+  const zoomLevel = 15
+  return (
+    <div className="googleMap">
+      <GoogleMapReact
+        bootstrapURLKeys={{key: process.env.NEXT_PUBLIC_GOOGLE_API_KEY}}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          _lat={location.lat}
+          _lng={location.lng}
+          _text={location.address}
+        />
+      </GoogleMapReact>
+    </div>
+  )
+}
--- alpha/components/visit.tsx
+++ alpha/components/visit.tsx
@@ -1,5 +1,12 @@
 import { FC } from "react"
+import { useRouter } from "next/router"
+import Image from "next/image"
+import { PortableText } from "@portabletext/react"
+import { components } from "components/portableTextComponents"
+import { urlFor } from "lib/utils"
+import { GoogleMap } from "components/googleMap"
 import { Layout } from "components/layout"
+import { LinkTo } from "components/linkTo"
 import {
   Label,
   Navigation,
@@ -8,6 +15,8 @@ import {
   PageContext,
   Settings
 } from "lib/interfaces"
+import s from "styles/visit.module.scss"
+import u from "styles/utils.module.scss"
 
 interface Props {
   labels:Label[]
@@ -36,7 +45,52 @@ export const Visit: FC<Props> = ({
       pageContext={pageContext}
       settings={settings}
     >
-      <div>{page.title}</div>
+      <section className={`${s.visitContainer} ${u.mAuto}`}>
+        <h1>{page.title} </h1>
+        <p className={`${s.subTitle}`}>{page.subtitle} </p>
+        <div className={`${s.spacesGrid} ${u.grid}`}>
+          {page.spaces && page.spaces.map(space => (
+            <LinkTo
+              href={`#${space.slug}`}
+              key={space._id}
+              style={{ margin: 0 }}
+            >
+              <Image
+                src={urlFor(space.mainImage)
+                  .width(468)
+                  .height(468)
+                  .auto("format")
+                  .quality(75)
+                  .url()}
+                alt={space.title}
+                width={2000}
+                height={2000}
+              />
+              {space.title &&
+                <div className={`${s.gridCaption}`}>{space.title}</div>
+              }
+            </LinkTo>
+          ))}
+        </div>
+        <div className={`${s.spacesTextGrid} ${u.grid}`}>
+          {page.spaces.map(space => (
+            space && (
+              <div
+                id={space.slug}
+                style={{ margin: 0 }}
+                key={space._id}
+                className={`${s.spacesText}`}
+              >
+                {space.title && <h4>{space.title}</h4>}
+                {space.body &&
+                  <PortableText value={space.body} components={components} />
+                }
+              </div>
+            )
+          ))}
+        </div>
+      </section>
+      {/* <GoogleMap /> */}
     </Layout>
   )
 }
