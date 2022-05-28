7add6a0b1eb0061f42f517fe0a7cb7b9f4fc071b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-28 16:20:46 +0100

    refactor: convert all styles on videos page to modules

--- alpha/components/videos.tsx
+++ alpha/components/videos.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react"
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { buildUrl, urlFor } from "lib/utils"
 import { Layout } from "components/layout"
+import { LinkTo } from "components/linkTo"
 import {
   Label,
   Navigation,
@@ -9,6 +12,8 @@ import {
   PageContext,
   Settings
 } from "lib/interfaces"
+import s from "styles/videos.module.scss"
+import u from "styles/utils.module.scss"
 
 interface Props {
   labels:Label[]
@@ -38,7 +43,36 @@ export const Videos: FC<Props> = ({
       pageContext={pageContext}
       settings={settings}
     >
-      <div>{page.title}</div>
+      <div className={`${s.sidebarContainer} ${u.grid}`}>
+        <div className={`${s.portableContainer}`}>
+          {page.title && <h1>{page.title}</h1>}
+          {page.subtitle &&
+            <p className={`${s.subTitle}`}>{page.subtitle}</p>
+          }
+        </div>
+      </div>
+      <div className={`${s.imageGrid} ${u.grid}`}>
+        {page.videos && page.videos.map(video => video &&
+          <div key={video._id} style={{ margin: 0 }}>
+            <LinkTo href={buildUrl(locale, video.slug, video._type)}>
+              <Image
+                src={urlFor(video.mainImage)
+                  .width(468)
+                  .height(468)
+                  .auto("format")
+                  .quality(75)
+                  .url()}
+                alt={video.title}
+                width={2000}
+                height={2000}
+              />
+              {video.title &&
+                <div className={`${s.gridCaption}`}>{video.title}</div>
+              }
+            </LinkTo>
+          </div>
+        )}
+      </div>
     </Layout>
   )
 }
