412cfb5b6fb4eccc50b224a416f909b8cb8ecaa3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-30 11:05:41 +0100

    build: remove old versions and prepare for test deploy

--- beta/README.md
+++
@@ -1,5 +0,0 @@
-# Mid Wales Arts
-
-Frontend development for [midwalesarts.org.uk](http://midwalesarts.org.uk).
-
-Made with [Next.js](https://www.nextjs.org/).
--- beta/lib/sanityClient.ts
+++
@@ -1,8 +0,0 @@
-import sanityClient from "@sanity/client"
-
-export default sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: "2021-03-25",
-  useCdn: true
-})
