33fe9b5bf5906cc26879601378997a759543fd81
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-06 20:56:30 +0100

    refactor: remove dark mode and add vercel widget

--- studio/src/dashboard/index.ts
+++ studio/src/dashboard/index.ts
@@ -1,8 +1,11 @@
 export default {
   widgets: [
-    // {
-    //   name: 'sanity-tutorials'
-    // },
+    {
+      name: 'vercel',
+      layout: {
+        width: 'full'
+      },
+    },
     {
       name: 'project-info'
     },
