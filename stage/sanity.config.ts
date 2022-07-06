3dbaae97fbb4386765f52bb23d05417914cfb69b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-06 13:27:29 +0100

    refactor: start to set up document i18n in v3

--- stage/sanity.config.ts
+++ stage/sanity.config.ts
@@ -1,8 +1,13 @@
 import {createConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {structure, defaultDocumentNode} from './src/structure'
-import {schemaTypes} from './src/schemas'
 import {dashboardTool, projectUsersWidget, projectInfoWidget} from '@sanity/dashboard'
+import {
+  documentI18n,
+  getDocumentList,
+  withDocumentI18nPlugin,
+} from '@sanity/document-internationalization'
+import {schemaTypes} from './src/schemas'
+import {defaultDocumentNode} from './src/structure'
 
 export default createConfig({
   name: 'default',
@@ -11,15 +16,42 @@ export default createConfig({
   projectId: 'atuc1oy0',
   dataset: 'production',
 
-  plugins: [
-    dashboardTool({
-      widgets: [projectInfoWidget(), projectUsersWidget()],
-    }),
-    deskTool({
-      defaultDocumentNode,
-      structure,
-    }),
-  ],
+  plugins: withDocumentI18nPlugin(
+    (pluginConfig) => [
+      documentI18n({
+        base: 'en',
+        languages: [
+          {
+            title: 'English',
+            id: 'en',
+          },
+          {
+            title: 'Welsh',
+            id: 'cy',
+          },
+        ],
+        idStructure: 'delimiter',
+        referenceBehavior: 'strong',
+        withTranslationsMaintenance: false,
+        fieldNames: {
+          lang: '__i18n_lang',
+          references: '__i18n_refs',
+          baseReference: '__i18n_base',
+        },
+      }),
+      dashboardTool({
+        widgets: [projectInfoWidget(), projectUsersWidget()],
+      }),
+      deskTool({
+        defaultDocumentNode,
+        structure: (S, {schema}) => getDocumentList({S, schema, config: pluginConfig}),
+      }),
+    ],
+    {
+      includeDeskTool: false,
+      // .. your i18n config
+    }
+  ),
 
   schema: {
     types: schemaTypes,
