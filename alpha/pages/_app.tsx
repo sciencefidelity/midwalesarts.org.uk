fac9fc6bb49f8e32ad7b17411178ca68c06a7e0b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-14 12:34:27 +0100

    refactor: add plausible script

--- alpha/pages/_app.tsx
+++ alpha/pages/_app.tsx
@@ -1,9 +1,18 @@
 import type { AppProps } from "next/app"
+import Script from "next/script"
 import "modern-normalize"
 import "styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://plausible.io/js/plausible.js"
+        data-domain="midwalesarts.org.uk"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
