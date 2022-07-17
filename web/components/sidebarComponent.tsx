6f6f41c5b22a6d5b01623a65eaec7959711bd1e9
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-17 16:51:50 +0100

    fix: change paths to root

--- web/components/sidebarComponent.tsx
+++ web/components/sidebarComponent.tsx
@@ -20,81 +20,100 @@ export const SidebarComponent: FC<Props> = ({ labels, sidebar }) => {
       <div className={`${s.sidebarContent}`}>
         <h3 className={`${s.h3}`}>{labels[20].text}</h3>
         <ul className={`${s.sidebarMenu}`}>
-          {sidebar.events && sidebar.events.map(event =>
-            event && (
-              <LinkTo
-                href={buildUrl(locale, event.slug, event._type)}
-                key={event._id}
-              >
-                <li className={`${s.listItem}`}>
-                  {event.date && <SidebarEventDate date={event.date} />}<br />
-                  {event.title &&
-                    <h4 className={`${s.listHeading}`}>{event.title}</h4>
-                  }
-                </li>
-              </LinkTo>
-            )
-          )}
+          {sidebar.events &&
+            sidebar.events.map(
+              event =>
+                event && (
+                  <LinkTo
+                    href={`/${buildUrl(locale, event.slug, event._type)}`}
+                    key={event._id}
+                  >
+                    <li className={`${s.listItem}`}>
+                      {event.date && <SidebarEventDate date={event.date} />}
+                      <br />
+                      {event.title && (
+                        <h4 className={`${s.listHeading}`}>{event.title}</h4>
+                      )}
+                    </li>
+                  </LinkTo>
+                )
+            )}
         </ul>
         <h3 className={`${s.h3}`}>{labels[21].text}</h3>
         <ul className={`${s.sidebarMenu}`}>
-          {sidebar.exhibitions && sidebar.exhibitions.map(exhibition =>
-            exhibition && (
-              <LinkTo
-                href={buildUrl(locale, exhibition.slug, exhibition._type)}
-                key={exhibition._id}
-              >
-                <li className={`${s.listItem}`}>
-                  {exhibition.dateStart &&
-                    <SidebarExhibitionDate
-                      dateEnd={exhibition.dateEnd}
-                      dateStart={exhibition.dateStart}
-                    />
-                  }<br />
-                  {exhibition.title &&
-                    <h4 className={`${s.listHeading}`}>{exhibition.title}</h4>
-                  }
-                </li>
-              </LinkTo>
-            )
-          )}
+          {sidebar.exhibitions &&
+            sidebar.exhibitions.map(
+              exhibition =>
+                exhibition && (
+                  <LinkTo
+                    href={`/${buildUrl(
+                      locale,
+                      exhibition.slug,
+                      exhibition._type
+                    )}`}
+                    key={exhibition._id}
+                  >
+                    <li className={`${s.listItem}`}>
+                      {exhibition.dateStart && (
+                        <SidebarExhibitionDate
+                          dateEnd={exhibition.dateEnd}
+                          dateStart={exhibition.dateStart}
+                        />
+                      )}
+                      <br />
+                      {exhibition.title && (
+                        <h4 className={`${s.listHeading}`}>
+                          {exhibition.title}
+                        </h4>
+                      )}
+                    </li>
+                  </LinkTo>
+                )
+            )}
         </ul>
         <h3 className={`${s.h3}`}>{labels[22].text}</h3>
         <ul className={`${s.sidebarMenu}`}>
-          {workshops && workshops.map(workshop =>
-            workshop && (
-              <LinkTo
-                href={buildUrl(locale, workshop.slug, workshop._type)}
-                key={workshop._id}
-              >
-                <li className={`${s.listItem}`}>
-                  <SidebarEventDate
-                    date={nextDate(dayToNumber(workshop.day), workshop.frequency)}
-                  />
-                  {workshop.title && <h4 className={`${s.listHeading}`}>
-                    {workshop.title}
-                  </h4>}
-                </li>
-              </LinkTo>
-            )
-          )}
+          {workshops &&
+            workshops.map(
+              workshop =>
+                workshop && (
+                  <LinkTo
+                    href={`/${buildUrl(locale, workshop.slug, workshop._type)}`}
+                    key={workshop._id}
+                  >
+                    <li className={`${s.listItem}`}>
+                      <SidebarEventDate
+                        date={nextDate(
+                          dayToNumber(workshop.day),
+                          workshop.frequency
+                        )}
+                      />
+                      {workshop.title && (
+                        <h4 className={`${s.listHeading}`}>{workshop.title}</h4>
+                      )}
+                    </li>
+                  </LinkTo>
+                )
+            )}
         </ul>
         <h3 className={`${s.h3}`}>{labels[19].text}</h3>
         <ul className={`${s.sidebarMenu}`}>
-          {sidebar.posts && sidebar.posts.map(post =>
-            post && (
-              <LinkTo
-                href={buildUrl(locale, post.slug, post._type)}
-                key={post._id}
-              >
-                <li className={`${s.listItem}`}>
-                  {post.title &&
-                    <h4 className={`${s.listHeading}`}>{post.title}</h4>
-                  }
-                </li>
-              </LinkTo>
-            )
-          )}
+          {sidebar.posts &&
+            sidebar.posts.map(
+              post =>
+                post && (
+                  <LinkTo
+                    href={`/${buildUrl(locale, post.slug, post._type)}`}
+                    key={post._id}
+                  >
+                    <li className={`${s.listItem}`}>
+                      {post.title && (
+                        <h4 className={`${s.listHeading}`}>{post.title}</h4>
+                      )}
+                    </li>
+                  </LinkTo>
+                )
+            )}
         </ul>
       </div>
     </aside>
