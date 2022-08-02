ac315036ce26168db6b86cd4b08ae3c3dabb5b83
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-08-02 13:07:28 +0100

    chore: upgrade studio

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import {createCliConfig} from 'sanity/cli'
 
 export default createCliConfig({
   api: {
-    projectId: import.meta.env.SANITY_STUDIO_API_PROJECT_ID,
-    dataset: import.meta.env.SANITY_STUDIO_API_PROJECT_DATASET,
+    projectId: '',
+    dataset: 'production',
   },
 })
