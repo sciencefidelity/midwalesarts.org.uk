56ae6179404c54759be8e4eb1aa35248c1c3d306
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-09-24 16:47:43 +0100

    refactor: cleanup

--- reinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# delete all node modules and reinstall after dependabot
-rm -rf ./node_modules yarn.lock &&
-yarn &&
-rm -rf */node_modules yarn.lock &&
-for d in */; do cd "$d"; yarn; cd ..; done
